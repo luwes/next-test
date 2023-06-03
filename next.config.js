e0caad07057f88a2f0b874c22c5e6610ec0e30ca
Wesley Luyten <me@wesleyluyten.com>
2023-06-03 10:15:36 -0500

    import cdn link on edge?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    urlImports: ['https://cdn.jsdelivr.net'],
+  },
 }
 
 module.exports = nextConfig
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+
+  const mediaChrome = await import('https://cdn.jsdelivr.net/npm/media-chrome@1/+esm');
+  console.log(mediaChrome);
+
   res.status(200).json({ name: 'John Doe' })
 }
